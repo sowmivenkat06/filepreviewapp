1b65ee26efedfcd69ee09797e3ab06f1d8287108
Sowmiya M V <155516853+sowmivenkat06@users.noreply.github.com>
2025-06-27 00:12:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
+  preview: {
+    host: true,
+    port: 4173, // optional
+    allowedHosts: ['filepreviewapp-1.onrender.com'] 
+  }
 });
