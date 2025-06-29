7183b8a007fa962b0c10d9407d0534c98310fe79
Sowmiya M V <155516853+sowmivenkat06@users.noreply.github.com>
2025-06-29 21:46:26 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,11 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-// 🔴 Replace this with your actual Client ID from Google Cloud Console
-const clientId = '473065953906-21s2ib6lk6mndoeq5for0cj76gkjbfq1.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
